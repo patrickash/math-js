f4a2c4449e0936ab3375020044852bbea321f384
Patrick Ashamalla <67871+patrickash@users.noreply.github.com>
2022-01-23 21:22:37 -0500

    change filename

--- example/index.js
+++ example/index.js
@@ -1,7 +1,8 @@
-import MathUtilities from '../src/MathUtilities.js';
+import MathUtilities from '../src/math-utilities.js';
 
 const arr = [12,2342,123,52,235,63,122];
 
 document.querySelector('.example').innerHTML = `
-  <p>MathUtilities.tanD: ${MathUtilities.tanD(23)}</p>
-  <p>MathUtilities.mean: ${MathUtilities.mean(arr)}</p>`;
+  <p><strong>tanD:</strong> ${MathUtilities.tanD(23)}</p>
+  <p><strong>mean:</strong> ${MathUtilities.mean(arr)}</p>
+  <p><strong>range:</strong> ${MathUtilities.range(arr)}</p>`;
